81929ff485ad782a48ed2f471a4d49a6df6f95c2
leeseungjae97 <sky_battle@naver.com>
2023-05-25 02:19:24 +0900

    Revert "Revert "0525""
    This reverts commit 119c064c622da4c0085a6f4bb05d8d83b6abaf7b.

--- diablo2/engine_source/mApplication.cpp
+++ diablo2/engine_source/mApplication.cpp
@@ -1,51 +1,70 @@
 #include "mApplication.h"
+#include "mInput.h"
+#include "mTime.h"
+#include "mRenderer.h"
 
 namespace m
 {
 	Application::Application()
-		: graphicsDevice(nullptr)
+		: graphicDevice(nullptr)
 		, mHwnd(NULL)
 		, mWidth(-1)
 		, mHeight(-1)
 	{
 
 	}
+
 	Application::~Application()
-	{
-	}
+	{}
+
 	void Application::Run()
 	{
 		Update();
 		LateUpdate();
 		Render();
 	}
+
 	void Application::Initialize()
 	{
+		Time::Initiailize();
+		Input::Initialize();
+
+		renderer::Initialize();
 	}
+
 	void Application::Update()
 	{
+		Time::Update();
+		Input::Update();
 	}
+
 	void Application::LateUpdate()
-	{
-	}
+	{}
+
 	void Application::Render()
 	{
-		graphicsDevice->Draw();
+		Time::Render();
+
+		graphicDevice->Draw();
 	}
+
 	void Application::SetWindow(HWND hwnd, UINT width, UINT height)
 	{
-		if (nullptr == graphicsDevice)
+		if (graphicDevice == nullptr)
 		{
 			mHwnd = hwnd;
 			mWidth = width;
 			mHeight = height;
 
-			graphicsDevice = std::make_unique<m::graphics::GraphicsDevice_DX11>();
+			graphicDevice = std::make_unique<m::graphics::GraphicDevice_DX11>();
+			m::graphics::GetDevice() = graphicDevice.get();
 		}
+
 		RECT rt = { 0, 0, (LONG)width , (LONG)height };
 		AdjustWindowRect(&rt, WS_OVERLAPPEDWINDOW, false);
 		SetWindowPos(mHwnd, nullptr, 0, 0, rt.right - rt.left, rt.bottom - rt.top, 0);
 		ShowWindow(mHwnd, true);
 		UpdateWindow(mHwnd);
 	}
+
 }
