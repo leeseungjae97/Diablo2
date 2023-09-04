1b2c7c54b6f0e379f4889c09f45eb69fe51ab969
leeseungjae97 <sky_battle@naver.com>
2023-09-04 16:47:56 +0900

    field item resource

--- diablo2/Engine/mFieldItem.cpp
+++ diablo2/Engine/mFieldItem.cpp
@@ -1,11 +1,22 @@
 #include "mFieldItem.h"
+
+#include "../engine_source/mMeshRenderer.h"
+
 namespace m
 {
 	FieldItem::FieldItem(eItem item)
 		: Item(item)
-	{}
+	{
+		ADD_COMP(this, MeshRenderer);
+		ADD_COMP(this, Collider2D);
+
+		SET_MESH(this, L"RectMesh");
+
+	}
 	FieldItem::~FieldItem()
-	{}
+	{
+	    
+	}
 	void FieldItem::Initialize()
 	{}
 	void FieldItem::Update()
